6d305183607d4f4ac66f341c0426fd2c2c069363
Anton Kalinichenko <kalina.heavy89@gmail.com>
2023-11-29 19:13:21 +0200

    Improved post list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,10 +5,15 @@ import { Context } from './index';
 import {observer} from "mobx-react-lite";
 import {IPost} from './models/IPost';
 import PostService from './services/PostService';
+import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
+import './styles/App.css';
+import PostList from './components/PostList';
 
 const App: FC = () => {
   const {store} = useContext(Context);
   const [posts, setPosts] = useState<IPost[]>([]);
+  const [value, setValue] = useState('Text in input');
 
   useEffect(() => {
     if (localStorage.getItem('access_token')) {
@@ -43,22 +48,32 @@ const App: FC = () => {
   }
 
   return (
-    <div>
+    <div className='App'>
       <h1>{ store.isAuth ? `Hello, ${store.user.name}!` : 'PLEASE AUTHENTICATE!' }</h1>
       <h1>{store.user.email_verified_at ? 'Email has been confirmed' : 'PLEASE CONFIRM YOUR EMAIL!'}</h1>
       {!store.user.email_verified_at && <div>
         <button onClick={store.sendEmailConfirmationNotification}>Resend Email Confirmation</button>
       </div>}
       <button onClick={() => store.logout()}>Log Out</button>
+
+      <div>
+        <h3>{value}</h3>
+        <input
+          type="text"
+          value={value}
+          onChange={event => setValue(event.target.value)}
+        />
+        <Counter />
+        <ClassCounter />
+      </div>
+
       <div>
         <button onClick={getPosts}>Get Posts</button>
       </div>
-      {posts.map(post => (
-        <div key={post.id}>
-          <div>{post.title}</div>
-          <div>{post.body}</div>
-        </div>
-      ))}
+
+      {typeof posts !== 'undefined' && posts.length > 0 &&
+        <PostList posts={posts} title="Post List" />
+      }
     </div>
   );
 }
